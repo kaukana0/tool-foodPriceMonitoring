40620d66ea57f4d6cf46e9f9c111ab1e1fcbebc8
robert <kaukana0@tuta.io>
2024-06-12 16:39:25 +0200

    add back processorCountryColors, updated w/ most recent order and as part of this, not as submodule some more little corrections

---
+++ app/components/processorCountryColors/countryColors.mjs
@@ -0,0 +1,41 @@
+export function process(inputData, output) {
+    output.colorPalette = [
+        // "#388AE2",  too similar to EU blue
+        "#B38F00",
+        "#208486",
+        "#AA5F18",
+        "#672DC4",
+        "#007243",
+        "#982630",
+        "#17256B",
+        "#005C99",
+        "#CCB151",
+        "#00525C",
+        "#803F02",
+        "#E04040",
+        "#33A033",
+        "#AF155C",
+        "#3A1380",
+        "#B656BD",
+        "#806300",
+        "#5FADAD",
+        "#CC833E",
+        "#7F1148",
+        "#1A4C1A",
+        "#672DC4",
+        "#14212E",
+        "#FFA3A3",
+        "#FFE066",
+        "#00E9FF",
+        "#DFB18B",
+        "#E0A8A8",
+        "#A2DDB2",
+        "#AB9CFF"
+    ]
+
+    output.countryColors = {
+        EU: "#0E47CB",
+        EA: "#F56426",
+    }
+}
+
