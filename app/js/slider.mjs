485f8d23b92ea93b885d30ae38a785e3b8929ceb
robert <kaukana0@tuta.io>
2024-08-08 15:18:47 +0200

    correction "DECember"

--- app/js/slider.mjs
+++ app/js/slider.mjs
@@ -21,7 +21,7 @@ export function init(data, left, max, onSelect) {
 export function update(leftText, rightText) {
 	const text = {
 		"01":"JAN", "02":"FEB", "03":"MAR", "04":"APR", "05":"MAY", "06":"JUN",
-		"07":"JUL", "08":"AUG", "09":"SEP", "10":"OCT", "11":"NOV", "12":"DEZ"}
+		"07":"JUL", "08":"AUG", "09":"SEP", "10":"OCT", "11":"NOV", "12":"DEC"}
 	const l = text[leftText.substring(0,2)] + " "
 	const r = text[rightText.substring(0,2)] + " "
 	document.getElementById("timeRange").setAttribute("textl", l+leftText.slice(3))
